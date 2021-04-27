b581af0c055bf6c011d9846d50597b4c460a12c9
AB LabDesk <bodein.antoine@gmail.com>
2021-04-26 20:38:12 -0400

    first

---
+++ data-raw/building_databases.R
@@ -0,0 +1,21 @@
+# General database setup
+
+library(tidyverse) 
+library(igraph)
+
+########################################
+##               BIOGRID              ##
+########################################
+ 
+# Release Version: 4.3.196
+# File: BIOGRID-ALL-4.3.196.tab3.zip 
+biogrid.tab <- read_tsv("./data-raw/databases/BIOGRID-ALL-4.3.196.tab3.zip")
+biogrid.tab.filtered <- biogrid.tab %>% 
+    dplyr::select("Official Symbol Interactor A", "Official Symbol Interactor B") %>% 
+    unique %>%
+    set_names(c("from", "to")) 
+
+BIOGRID <- graph_from_data_frame(biogrid.tab.filtered, directed = FALSE) %>% simplify()
+usethis::use_data(BIOGRID)
+
+## TTRUST
